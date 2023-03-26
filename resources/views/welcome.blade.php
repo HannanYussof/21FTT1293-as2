ebcf791f29834ead9cb9bcd9036438272dffc25e
HannanYussof <98924955+HannanYussof@users.noreply.github.com>
2023-03-26 11:31:40 +0800

    Changed student ID to AS2
    Student ID on line 36 was changed to AS2

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
-                       <h1 style="color: red;"> 21FTT1293 </h1>
+                       <h1 style="color: red;"> AS2 </h1>
                 </div>
 
                 <div class="mt-16">
