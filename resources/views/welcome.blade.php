fb1da45fb435bfbd02746e227173be7e37a672cc
Hannan Yussof <21FTT1293@student.pb.edu.bn>
2023-03-26 11:45:35 +0800

    Change AS2 to Done

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -33,7 +33,7 @@
 
             <div class="max-w-7xl mx-auto p-6 lg:p-8">
                 <div class="flex justify-center">
-                       <h1 style="color: red;"> AS2 </h1>
+                       <h1 style="color: red;"> Done </h1>
                 </div>
 
                 <div class="mt-16">
